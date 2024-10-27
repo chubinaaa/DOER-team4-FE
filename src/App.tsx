907a8077f0cc0d5787de5964f5f137988c7b4a37
Irakli Pankvelashvili <irakli.pankvelashvili@gmail.com>
2024-10-27 16:23:34 +0400

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 function App() {
   const a = 2
-  return <h1 className="text-blue-700 text-3xl text-center"><h2>asdasd</h2>hello world!</h1>;
+  return <h1 className="text-blue-700 text-3xl text-center">                    
+  <h2>asdasd</h2>hello world!</h1>;
 }
 
 export default App;
