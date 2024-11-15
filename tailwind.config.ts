b42809784655c106eb6f1d3be5394d47b27256c6
Irakli Pankvelashvili <irakli.pankvelashvili@gmail.com>
2024-11-15 14:54:30 +0400

    2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
     content: [
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,9 +8,11 @@ const config: Config = {
     ],
     theme: {
         extend: {
-            // Additional custom classes goes here ...
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
         },
     },
     plugins: [],
-};
-export default config;
+} satisfies Config;
